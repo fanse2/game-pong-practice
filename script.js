80e0af900b0ef971f9f7c92ddf698485f5a32d7c
June Choi <fanse2@gmail.com>
2022-03-01 16:08:01 -0800

    2nd

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-// Update loop
+import Ball from './Ball.js'
+import Paddle from './Paddle.js'
+
+const ball = new Ball(document.querySelector('#ball'))
+const playerPaddle = new Paddle(document.querySelector('#player-paddle'))
+const computerPaddle = new Paddle(document.querySelector('#compuer-paddle'))
+const playerScoreElem = document.querySelector('#player-score')
+const computerScoreElem = document.querySelector('#computer-score')
+
+
+let lastTime
+
+function update(time){
+    if (lastTime != null){
+        const delta = time - lastTime
+
+        ball.update(delta,[playerPaddle.rect(), computerPaddle.rect()])
+        computerPaddle.update(delta, ball.y)
+        const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--hue'))
+
+        document.documentElement.style.setProperty('--hue',hue + delta * .01)
+
+        if(isLose()) {
+            handelLose()
+        }
+    }
+    
+    lastTime = time
+    window.requestAnimationFrame(update)
+}
+
+function isLose() {
+    const rect = ball.rect()
+    return rect.right >= window.innerWidth || rect.left <= 0
+}
+
+function handelLose() {
+    const rect = ball.rect()
+
+    if(rect.right >= window.innerWidth) {
+        playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+    } else {
+        computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+    }
+
+
+    ball.reset()
+}
+
+document.addEventListener('mousemove', e =>{
+    playerPaddle.position = (e.y / window.innerHeight) * 100
+})
+
+window.requestAnimationFrame(update)
